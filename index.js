38b464c9c53467af975646017547174df186972f
Ms Eko <jomaduagwu@gmail.com>
2023-04-26 22:27:16 -0500

    added functions to save svg logo file and init app

--- index.js
+++ index.js
@@ -1,5 +1,27 @@
+const inquirer = require('inquirer');
+const fs = require('fs');
 const questions = require('./lib/questions.js');
+const generateLogo = require('./lib/generateLogo');
 
-const main = new questions();
+// const main = new questions();
 
-main.run();
+// main.run();
+
+// create svg logo file
+function writeToFile(fileName, data) {
+    fs.writeFile(fileName, data, (err) =>
+    err  ? console.log(err)  : console.log('Successfully created new logo!')
+    );
+}
+
+
+// function to initialize app
+function init() {
+    inquirer.prompt(questions).then((data) => {
+        const logoContent = generateLogo(data);
+        writeToFile('ExampleLogo.svg', logoContent);
+    });
+}
+
+// initialize app
+init()
