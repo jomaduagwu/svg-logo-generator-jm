a702502010186c6f683ef77292a6f01d87b9fb7d
Ms Eko <jomaduagwu@gmail.com>
2023-04-30 13:22:22 -0500

    updated code

--- index.js
+++ index.js
@@ -3,9 +3,42 @@ const fs = require('fs');
 const questions = require('./lib/questions.js');
 const generateLogo = require('./lib/generateLogo');
 
-// const main = new questions();
-
-// main.run();
+const questions = [
+    {
+        type: 'list',
+        name: 'shape',
+        message: 'What shape would you like?',
+        choices: ['Cirlce', 'Square', 'Triangle'],
+    },
+    {
+        type: 'list',
+        name: 'shapeColor',
+        message: 'Pick a shape color.',
+        choices: [''],
+    },
+    {
+        type: 'input',
+        name: 'text',
+        message: 'What text would you like (it has to be 3 characters only)?',
+        validate: function (value) {
+            if (value.length > 3) {
+                return "Please enter only 3 characters.";
+            } else {
+                if (value.length < 3) {
+                    return "Please enter up to 3 characters.";
+                };
+                return true;
+            }
+        }
+    },
+    {
+        type: 'list',
+        name: 'textColor',
+        message: 'What color of text would you like?',
+        choices: [''],
+    }
+    
+];
 
 // create svg logo file
 function writeToFile(fileName, data) {
